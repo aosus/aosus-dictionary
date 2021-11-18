d226ae5d9e48d0af804c94320df58d37608a0db2
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2021-11-18 13:05:18 +0300

    Update default.vue

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <Nuxt class=" min-h-1/1 flex-1"/>
+    <Nuxt class="min-h-1/1 flex-1" />
     <Footer />
   </div>
 </template>
@@ -11,6 +11,8 @@
 #app {
   min-height: 100vh;
   font-family: "Almarai", -apple-system, Helvetica, Arial, sans-serif;
+  background: #031e03;
+  color: white;
   @apply flex flex-col;
 }
 </style>
